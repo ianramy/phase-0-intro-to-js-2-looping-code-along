ea3a1e8659db934d8f3b0cb8f997f5c75970cee7
ianramy <imwagore@gmail.com>
2024-06-27 11:55:09 +0300

    origin/master

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, giftType) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a number and log each step
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
